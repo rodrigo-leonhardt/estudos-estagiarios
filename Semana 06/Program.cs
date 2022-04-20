6c083b2ecb763be94fa34f4495360d50a135d6db
Rodrigo Leonhardt <rodrigo_leonhardt@hotmail.com>
2022-04-20 14:11:11 -0300

    .

---
+++ Semana 06/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ExemploLinq
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {            
+            Console.WriteLine("1. Fornecedores ordenados por Nome");
+
+            //Início o trecho para alteração
+            var fornecedoresOrdenados = ;
+            //Fim do trecho para alteração
+
+            foreach(var fornecedor in fornecedoresOrdenados)
+                Console.WriteLine("- Codigo: {0} Nome: '{1}'", fornecedor.Codigo, fornecedor.Nome);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("2. Notas Fiscais do fornecedor 2");
+
+            //Início o trecho para alteração            
+            var notasFiscaisFornecedor2 = ;
+            //Fim do trecho para alteração
+
+            foreach(var notaFiscal in notasFiscaisFornecedor2)
+                Console.WriteLine("- Número: {0} Valor Total: {1:C}", notaFiscal.Numero, notaFiscal.ValorTotal);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("3. Valor total das notas por fornecedor");
+
+            //Início do trecho para alteração
+            var valorTotalNotasPorFornecedor = NotaFiscal.Lista.???;
+            //Fim do trecho para alteração
+
+            foreach(var item in valorTotalNotasPorFornecedor)
+                Console.WriteLine("- Fornecedor: '{0}' Valor Total: {1:C}", item.Fornecedor, item.Valor);
+            
+        }
+
+    }
+
+    public class Fornecedor
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+
+        public Fornecedor(int codigo, string nome)
+        {
+            Codigo = codigo;
+            Nome = nome;
+        }
+
+        public static IEnumerable<Fornecedor> Lista = new List<Fornecedor>() 
+        {
+            new Fornecedor(1, "Casa dos pregos"),
+            new Fornecedor(2, "Tintas Marta"),
+            new Fornecedor(3, "Supermercado Baratão")
+        };        
+    }
+
+    public class NotaFiscal
+    {
+        public int Numero { get; set; }        
+        public int CodigoFornecedor { get; set; }
+        public double ValorTotal { get; set; }
+
+        public static IEnumerable<NotaFiscal> Lista = new List<NotaFiscal>()
+        {
+            new NotaFiscal(1001, 1, 50),
+            new NotaFiscal(1002, 1, 65.5),
+            new NotaFiscal(1003, 2, 1428),
+            new NotaFiscal(1004, 3, 99.99),
+            new NotaFiscal(1005, 3, 67.8),
+            new NotaFiscal(1006, 3, 9.99)
+        };
+
+        public NotaFiscal(int numero, int codigoFornecedor, double valorTotal)
+        {
+            Numero = numero;
+            CodigoFornecedor = codigoFornecedor;
+            ValorTotal = valorTotal;
+        }
+
+    }
+
+}
---
+++ Semana 06/readme.md
@@ -0,0 +1,44 @@
+# Exercícios
+
+1) Altere os trechos destacados no arquivo **Program.cs** para que a execução resulte no seguinte:
+
+```
+   1. Fornecedores ordenados por Nome
+   - Codigo: 1 Nome: 'Casa dos pregos'
+   - Codigo: 3 Nome: 'Supermercado Baratão'
+   - Codigo: 2 Nome: 'Tintas Marta'
+
+   2. Notas Fiscais do fornecedor 2
+   - Número: 1003 Valor Total: R$ 1.428,00
+
+   3. Valor total das notas por fornecedor
+   - Fornecedor: 'Casa dos pregos' Valor Total: R$ 115,50
+   - Fornecedor: 'Tintas Marta' Valor Total: R$ 1.428,00
+   - Fornecedor: 'Supermercado Baratão' Valor Total: R$ 177,78
+```
+
+# Referências
+
+#### Introdução a consultas LINQ
+- https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/concepts/linq/introduction-to-linq-queries
+
+#### LINQ e tipos genéricos
+https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/concepts/linq/linq-and-generic-types
+
+#### Operações de consulta LINQ básica
+https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/concepts/linq/basic-linq-query-operations#grouping
+
+#### Sintaxe de consulta e sintaxe de método em LINQ
+https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/concepts/linq/query-syntax-and-method-syntax-in-linq
+
+#### C# - Introdução ao LINQ
+https://www.youtube.com/watch?v=ja1nORsswSU
+
+#### C# - LINQ - .Where() .OrderBy() .OrderByDescending() .Reverse() - Sintaxe de método
+https://www.youtube.com/watch?v=f9g_8HOXEBM
+
+#### C# Essentials: Linq for Lists - Sorting, Filtering, and Aggregating Lists Easily
+https://www.youtube.com/watch?v=yClSNQdVD7g
+
+#### Estudo adicional - LINQ (Consulta Integrada à Linguagem) - [Toda a seção]
+- https://docs.microsoft.com/pt-br/dotnet/csharp/linq/
